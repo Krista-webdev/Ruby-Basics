ed1ca9d2365b28e83b91b26e2a05fa0e55e3b82e
Krista Ogle <61994897+Krista-webdev@users.noreply.github.com>
2020-12-17 21:53:40 -0500

    add say.rb

---
+++ say.rb
@@ -0,0 +1,16 @@
+# Instead of repeating puts....
+# puts "Hello"
+# puts "Hi"
+# puts "How are you"
+# puts "I'm fine"
+
+def say(words)
+  puts words
+end
+
+say("Hello")
+say("Hi")
+say("How are you")
+say("I'm fine")
+
+# the program now has more flexibility
